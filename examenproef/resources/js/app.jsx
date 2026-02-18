6dc390923cc891338f1d2922aca4d27afdada1a9
KenKoorn <35050@ma-web.nl>
2026-02-18 11:53:37 +0100

    SCSS en images toegevoegd, begin van frontpagina.

--- examenproef/resources/js/app.jsx
+++ examenproef/resources/js/app.jsx
@@ -1,5 +1,8 @@
 import '../css/app.css';
 import './bootstrap';
+import '../scss/app.scss';
+
+
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
