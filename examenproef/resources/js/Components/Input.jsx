44eb8416bb8c3c532a010a4b8f5985e5c948781f
smohammed576 <smohammed0576@gmail.com>
2026-02-20 11:23:28 +0100

    forgot password function

--- examenproef/resources/js/Components/Input.jsx
+++ examenproef/resources/js/Components/Input.jsx
@@ -1,8 +1,9 @@
-const TextInput = ({label, placeholder, type, value, onChange}) => {
+const TextInput = ({label, placeholder, type, value, onChange, error}) => {
     return (
         <div className="textinput">
             <label className="textinput__label">{label}</label>
-            <input placeholder={placeholder} value={value} onChange={onChange} type={type == null ? 'text' : type} className="textinput__input" />
+            <input placeholder={placeholder} value={value} onChange={onChange} type={type == null ? 'text' : type} readOnly={onChange == null} className="textinput__input" />
+            <p className="textinput__error">{error}</p>
         </div>
     );
 }
